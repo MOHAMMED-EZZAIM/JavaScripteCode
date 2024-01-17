f5cdd6f5a2a5af48dcbe3bcc19682c2ed31fd03e
unknown <fromscratchleretour@gmail.com>
2024-01-17 15:23:18 +0100

    succes

---
+++ code1/index.js
@@ -0,0 +1,20 @@
+/*
+console.log(`hello word `);
+console.log(`i like piza`);
+window.alert('thid is a lert');
+window.alert('i like pizza');
+document.getElementById("myh1").textContent='hello word 999';
+
+let age=30;
+console.log(`your age est ${age}`);
+
+let firsname ="mohammed ezzaim ";
+console.log(`your name is ${firsname}`);
+
+let boleane=false;
+console.log(boleane);
+
+*/
+
+document.getElementById('myp').textContent="your are mohammed ezzam";
+
